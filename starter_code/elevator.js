5d524350eac761f7c45a352c200746ff286c171d
Rak <lovetacirupeca@gmail.com>
2017-12-18 07:47:10 +0100

    feat: add start and stop function

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,28 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    this.startInterval = setInterval(() => {this.update();}, 1000);
+  }
+  stop() {
+    clearInterval(this.startInterval);
+  }
+  update() {
+    this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,6 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator1 = new Elevator();
+
+elevator1.update();
